162a24ff21c8b8690046b086db7d3765a039dea1
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2025-03-31 12:56:06 +0530

    2.3

---
+++ achievment-2/2.3/bookstore.py
@@ -0,0 +1,6 @@
+# application is for the entire app for this achievment
+# app is for this specifc task!
+
+# the Bookstore application’s intended purpose is to 
+# manage and generate your (imaginary) bookstore’s information, 
+# such as sales figures and book and customer details.
